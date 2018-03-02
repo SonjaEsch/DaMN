ab027c9c55d5b118f0e5d10eb9bbe1ac5b7955af
Manuel Wiese <manuel.wiese@uni-muenster.de>
2018-03-02 17:15:42 +0100

    Refactored TestSpectrumCalculation.py.

--- TestSpectrumCalculation.py
+++ TestSpectrumCalculation.py
@@ -18,30 +18,8 @@ fermion = fermion_creator.create()
 neutrino_creator = Particle.ParticleCreator(NeutrinoCouplings.Neutrino, "configs/neutrino_couplings_test.json")
 neutrino = neutrino_creator.create()
 
-print(higgs)
-print(scalar)
-print(fermion)
-print(neutrino)
-
 model = ModelT12ANeutrinoCouplings.ModelT12A(higgs, fermion, scalar, neutrino)
-
-print("Model parameters")
-print(higgs)
-print(scalar)
-print(fermion)
-print(neutrino)
-
 
 model.calculate_dependent_variables()
 
-print("Higgs")
-model.higgs_dependent.pprint()
-
-print("Scalar")
-model.scalar_dependent.pprint()
-
-print("Fermion")
-model.fermion_dependent.pprint()
-
-print("Neutrino")
-model.neutrino_dependent.pprint()
+model.pprint()
